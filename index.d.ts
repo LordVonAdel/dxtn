3d3a67d8bef394c441d1c559dcd20824c1a3a7a2
LordVonAdel <lordvonadel@gmail.com>
2022-03-14 18:26:00 +0100

    changed attribute name for decompressing functions
    Signed-off-by: LordVonAdel <lordvonadel@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ export declare function compressDXT1(width: number, height: number, rgba8888: Ar
 export declare function compressDXT3(width: number, height: number, rgba8888: ArrayLike<number>);
 export declare function compressDXT5(width: number, height: number, rgba8888: ArrayLike<number>);
 
-export declare function decompressDXT1(width: number, height: number, rgba8888: ArrayLike<number>);
-export declare function decompressDXT3(width: number, height: number, rgba8888: ArrayLike<number>);
-export declare function decompressDXT5(width: number, height: number, rgba8888: ArrayLike<number>);
+export declare function decompressDXT1(width: number, height: number, compressedData: ArrayLike<number>);
+export declare function decompressDXT3(width: number, height: number, compressedData: ArrayLike<number>);
+export declare function decompressDXT5(width: number, height: number, compressedData: ArrayLike<number>);
--- index.js
+++ index.js
@@ -12,16 +12,16 @@ function compressDXT5(width, height, rgba8888) {
   return DXT.DXT5.compress(width, height, rgba8888);
 }
 
-function decompressDXT1(width, height, rgba8888) {
-  return DXT.DXT1.decompress(width, height, rgba8888);
+function decompressDXT1(width, height, compressedData) {
+  return DXT.DXT1.decompress(width, height, compressedData);
 }
 
-function decompressDXT3(width, height, rgba8888) {
-  return DXT.DXT3.decompress(width, height, rgba8888);
+function decompressDXT3(width, height, compressedData) {
+  return DXT.DXT3.decompress(width, height, compressedData);
 }
 
-function decompressDXT5(width, height, rgba8888) {
-  return DXT.DXT5.decompress(width, height, rgba8888);
+function decompressDXT5(width, height, compressedData) {
+  return DXT.DXT5.decompress(width, height, compressedData);
 }
 
 module.exports = {
